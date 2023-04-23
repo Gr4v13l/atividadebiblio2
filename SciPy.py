9ac8cd2e050021a1ad92fbb1baad7e74d7d76635
EC2 Default User <ec2-user@ip-172-31-93-152.ec2.internal>
2023-04-23 15:36:46 +0000

    Atividade Bibliotecas

---
+++ SciPy.py
@@ -0,0 +1,9 @@
+import numpy as np
+from scipy import io as sio
+ 
+array = np.ones((4, 4))
+sio.savemat('example.mat', {'ar': array}) 
+data = sio.loadmat('example.mat', struct_as_record=True)
+data['ar']
+
+# Professor, apesar do código aparentemente estar correto na hora de printar, não printa nada.
